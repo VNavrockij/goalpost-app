0526c0ded22dc165e91d377d8f7607505c3f760b
VNavrockij <vitalik.nav@gmail.com>
2022-05-15 10:39:08 +0300

    feat: start new task

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goalpost-app
 //
 //  Created by Vitalii Navrotskyi on 15.05.2022.
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tabelView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addGoalBtnWasPressed(_ sender: UIButton) {
+    }
+    
 }
 
